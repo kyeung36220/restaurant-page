156d05e8c06e73ea9d5d6254ebc678ab021c2f91
kyeung36220 <135488116+kyeung36220@users.noreply.github.com>
2025-01-08 11:56:24 -0500

    Menu page UI complete

--- src/menuPage.js
+++ src/menuPage.js
@@ -8,6 +8,7 @@ export function addMenuPage() {
     const content = document.querySelector("#content")
     const menuPageContainer = document.createElement("div")
     menuPageContainer.classList.add("menuPageContainer")
+    content.appendChild(menuPageContainer)
 
     //add character
     const asuka = document.createElement("img")
@@ -20,14 +21,63 @@ export function addMenuPage() {
     asukaShadow.src = asukaShadowPng
     menuPageContainer.appendChild(asukaShadow);
 
-    content.appendChild(menuPageContainer)
-
     //container for title and items
-    const menuItemsContainer = document.createElement("div")
-    menuItemsContainer.classList.add("menuItemsContainer")
-    menuPageContainer.appendChild(menuItemsContainer)
+    const menuTextContainer = document.createElement("div")
+    menuTextContainer.classList.add("menuTextContainer")
+    menuPageContainer.appendChild(menuTextContainer)
 
     //add title
     const menuTitle = document.createElement("div")
+    menuTitle.classList.add("menuTitle")
+    menuTitle.textContent = "MENU ITEMS"
+    menuTextContainer.appendChild(menuTitle)
 
+    //container for all items flex container
+    const menuItemsContainer = document.createElement("div")
+    menuItemsContainer.classList.add("menuItemsContainer")
+    menuTextContainer.appendChild(menuItemsContainer)
+
+    const onigiri = document.createElement("div")
+    onigiri.classList.add("onigiri", "item")
+    menuItemsContainer.appendChild(onigiri)
+
+    const eggBite = document.createElement("div")
+    eggBite.classList.add("eggBite", "item")
+    menuItemsContainer.appendChild(eggBite)
+
+    const tempura = document.createElement("div")
+    tempura.classList.add("tempura", "item")
+    menuItemsContainer.appendChild(tempura)
+
+    //menu items images
+    const onigiriImage = document.createElement("img")
+    onigiriImage.classList.add("onigiriImage", "image")
+    onigiriImage.src = onigiriPng
+    onigiri.appendChild(onigiriImage);
+
+    const eggBiteImage = document.createElement("img")
+    eggBiteImage.classList.add("eggBiteImage", "image")
+    eggBiteImage.src = eggBitePng
+    eggBite.appendChild(eggBiteImage);
+
+    const tempuraImage = document.createElement("img")
+    tempuraImage.classList.add("tempuraImage", "image")
+    tempuraImage.src = tempuraPng
+    tempura.appendChild(tempuraImage);
+
+    //menu items text
+    const onigiriText = document.createElement("div")
+    onigiriText.classList.add("onigiriText", "text")
+    onigiriText.textContent = "First Contact Onigiri"
+    onigiri.appendChild(onigiriText);
+
+    const eggBiteText = document.createElement("div")
+    eggBiteText.classList.add("eggBiteText", "text")
+    eggBiteText.textContent = "Lilith's Golden Bite"
+    eggBite.appendChild(eggBiteText);
+
+    const tempuraText = document.createElement("div")
+    tempuraText.classList.add("tempuraText", "text")
+    tempuraText.textContent = "Third Impact Tempura"
+    tempura.appendChild(tempuraText);
 }
